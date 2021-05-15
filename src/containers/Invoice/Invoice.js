8741aef6df736f0da6ce2d5bc7b3034d1ba716e0
Adarsh Srivastava <herecomesthebeastlesnar.com>
2021-05-15 22:34:05 +0530

    Added Products

--- src/containers/Invoice/Invoice.js
+++ src/containers/Invoice/Invoice.js
@@ -2,6 +2,8 @@ import styles from "./Invoice.module.css";
 import { useState, Fragment } from "react";
 import StatusButton from "../../components/StatusButton/StatusButton";
 import NormalInput from "../../components/NormaIInput/NormalInput";
+import Button from "../../components/Button/Button";
+import AddIcon from "../../assets/AddIcon.svg";
 
 const Invoice = (props) => {
   const [SIN, setSIN] = useState("");
@@ -39,6 +41,49 @@ const Invoice = (props) => {
           placeholder="Here will go the information entered by the user."
           width="20%"
         />
+      </div>
+      <div className={styles.SIN__field}>
+        <div className={styles.products__row}>
+          <div className={styles.products__upper}>
+            <div className={styles.products__heading}>Products</div>
+            <div className={styles.products__buttons}>
+              <StatusButton color="red" text="Delete" />
+              <Button
+                text="Add New"
+                disabled={false}
+                color={"#fff"}
+                bgColor={"#08AE54"}
+                icon={AddIcon}
+              />
+            </div>
+          </div>
+          <div className={styles.fields}>
+            <NormalInput
+              value={created}
+              setValue={setCreated}
+              placeholder="Here will go the"
+              width="15%"
+            />
+            <NormalInput
+              value={created}
+              setValue={setCreated}
+              placeholder="Here will go the"
+              width="20%"
+            />
+            <NormalInput
+              value={created}
+              setValue={setCreated}
+              placeholder="Here will go the"
+              width="10%"
+            />
+            <NormalInput
+              value={created}
+              setValue={setCreated}
+              placeholder="Here will go the"
+              width="10%"
+            />
+          </div>
+        </div>
       </div>
       <div className={styles.SIN__field}>
         <div>Status</div>
